3c2783ed8abf6544e53d59c75ff710a7e767acb3
Jp <joelpereirasegunda@gmail.com>
2025-08-11 20:12:54 +0100

    successfully implemented delete task button

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,10 @@
 const addTaskButton = document.querySelector("#add-task-button");
 const showTasks = document.querySelector("#tasks");
 
-
-
-
-
-//Create the tasks list to put in show tasks
-const divTaskTodo = document.createElement('div');
-const completedTask = document.createElement('input');
-const taskName = document.createElement('p');
-const iconDiv = document.createElement('div');
-const editIcon = document.createElement("i")
-const deleteIcon = document.createElement("i")
-
 // Defining the object that defines the class
 var currentTasks =[];
 var taskId = 1;
 
-
-//Events
 addTaskButton.addEventListener('click', (e) =>{
     e.preventDefault();
 
@@ -28,13 +14,32 @@ addTaskButton.addEventListener('click', (e) =>{
 
     AddTask(text);
     
+});
+
+showTasks.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    if (e.target.matches(".fa-trash")){
+        const taskId = e.target.dataset.id;
+        currentTasks = currentTasks.filter(task => task.id != taskId);
+        e.target.closest('#task-todo').remove();
+    }
 })
 
 //Function to add new tasks
 function AddTask(text){
     // It initializes an object with the given tasks
     currentTasks.push({id:taskId,name:text,completed:false});
-    taskId++; //update the id
+    
+
+    //Create the wrapper class for the list
+    const divTaskTodo = document.createElement('div');
+    const completedTask = document.createElement('input');
+    const taskName = document.createElement('p');
+
+    const iconDiv = document.createElement('div');
+    const editIcon = document.createElement("i")
+    const deleteIcon = document.createElement("i")
 
     //Append to get the structure
     divTaskTodo.appendChild(completedTask);
@@ -45,22 +50,30 @@ function AddTask(text){
 
     // Adding the classnames and Id's
     divTaskTodo.id = "task-todo";
+    divTaskTodo.className = `${taskId}`
     completedTask.id = "completed-task";
     completedTask.type = "checkbox";
 
     iconDiv.id = "task-actions";
+    deleteIcon.className = "fa-solid fa-trash";
+
     editIcon.className = "fa-solid fa-pen-to-square";
     editIcon.id = "edit-task";
-    deleteIcon.className = "fa-solid fa-trash";
+
     deleteIcon.id = "delete-task";
+    deleteIcon.dataset.id = taskId; //changed here
+
+    taskName.textContent = text;
 
     taskName.textContent = text;
 
     showTasks.appendChild(divTaskTodo);
     showTasks.className = "";
+    taskId++; //update the id
 }
 
 //Function Delete Button
+
 
 //Function Edit Button
 
